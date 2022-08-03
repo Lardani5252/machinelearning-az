bdffdfb1c21f00ffc875ab0084e7c9be04d534df
Lardani5252 <ignaciolardani52@gmail.com>
2022-08-02 22:24:12 -0300

    Create Sin título 0.py

---
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Sin título 0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 21:47:44 2022
+
+@author: ignac
+"""
+
+#Plantilla de pre procesado
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv("Data.csv")
